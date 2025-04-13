ba96ba59d4bad1eb76386a634f9ba1ab5e6dee76
jxphoh <jxphoh@outlook.com>
2025-04-14 00:03:24 +0800

    Unlock

--- scripts/_pkg/Const.py
+++ scripts/_pkg/Const.py
@@ -8,12 +8,12 @@ docmap_siyuan = {
     "Development": "Development",
 }
 
-repo_siyuan = "siyuan-note/siyuan"
+repo_siyuan = "jxphoh/siyuan"
 hostname = "api.github.com"
 
 HEADER_siyuan = '''
 '''
 
 HEADER = {
-  "siyuan-note/siyuan": HEADER_siyuan,
+  "jxphoh/siyuan": HEADER_siyuan,
 }
